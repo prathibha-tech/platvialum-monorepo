e6f06506ab098d004a0448724b1a5c1e0003733b
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2024-12-18 11:49:32 +0530

    chore : add monorepo

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className=" text-blue-500 flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <div className="text-center text-4xl text-white font-bold">
+        Hello, MonoRepo!
+      </div>
+    </div>
+  )
+}
